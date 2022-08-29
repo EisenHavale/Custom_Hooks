6a50793d8fe56656b6bfb3733a683284a86f35b1
Eisen Havale <eisenprogramando@gmail.com>
2022-08-29 08:57:52 -0500

    Working on compound cards, missing logic to decided what's redered

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -6,7 +6,6 @@ import { IButtonProp } from "../../../interfaces/atoms-interfaces";
 export const Button = ({ text, disabled = false, onClick }: IButtonProp) => {
   return (
     <button disabled={disabled} onClick={onClick}>
-      {" "}
       {text}
     </button>
   );
--- src/components/molecules/compoundCard/CompounCard.tsx
+++ src/components/molecules/compoundCard/CompounCard.tsx
@@ -1,15 +1,15 @@
-import React, { useContext} from 'react'
+import React, { useContext } from 'react'
 import { CompoundCardContext } from '../../../app/card-context/CardContext';
 import { ICardProps } from '../../../interfaces/molecules-interfaces';
 
 
 
 
-export const CompounCard = ({card, children}:ICardProps) => {
-    
-  return (
-    <CompoundCardContext.Provider value={card}>
-        {children}
-    </CompoundCardContext.Provider>
-  )
+export const CompounCard = ({ card, children }: ICardProps) => {
+
+    return (
+        <CompoundCardContext.Provider value={card}>
+            {children}
+        </CompoundCardContext.Provider>
+    )
 }
